cfa05e8fcf46c22ead4c97b86fa36faf180ffd82
Inakigarro <ignakigarro@gmail.com>
2025-10-28 18:29:30 -0300

    feat: Implement user profile management with form validation and tabbed interface

--- angular/zsport-ui/src/app/pages/profile/profile.component.ts
+++ angular/zsport-ui/src/app/pages/profile/profile.component.ts
@@ -27,6 +27,8 @@ import { UpdateUserRequest, User, UserRoles } from '@app/auth/types/auth.type';
 import { SnackbarService } from '@app/shared/snackbar/services/snackbar.service';
 import { AuthService } from '@app/auth/auth.service';
 import { MatChipsModule } from '@angular/material/chips';
+import { ProfileInfoComponent } from "./profile-info/profile-info.component";
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
 	selector: 'zs-profile',
@@ -40,160 +42,13 @@ import { MatChipsModule } from '@angular/material/chips';
 		MatCardModule,
 		MatInputModule,
 		MatIconModule,
-		MatLabel,
-		ButtonComponent,
 		MatProgressSpinnerModule,
 		MatChipsModule,
+		ProfileInfoComponent,
+		MatTabsModule
 	],
 })
 export class ProfileComponent {
 	@ViewChild('loadingRef', { static: true }) loadingRef: ElementRef;
-	private readonly loadingService = inject(LoadingService);
-	private readonly navigationService = inject(NavigationService);
-	private readonly snackbarService = inject(SnackbarService);
-	private readonly authService = inject(AuthService);
-	private readonly authStore = inject(AuthStore);
-	private destroy$ = new Subject<void>();
-	private isFormValid = signal<boolean>(false);
-
-	protected username: string | null = null;
-	protected user: User | null = null;
-
-	protected nombreControl = new FormControl('', [Validators.required]);
-	protected apellidoControl = new FormControl('', [Validators.required]);
-	protected emailControl = new FormControl('', [
-		Validators.required,
-		Validators.email,
-	]);
-
-	protected saveButton = computed<Button>(() => ({
-		id: 'save',
-		label: 'Guardar',
-		icon: 'save',
-		buttonType: 'raised',
-		htmlType: 'submit',
-		disabled: !this.isFormValid(),
-		action: () => this.onSaveButtonClicked(),
-	}));
-	protected cancelButton = computed<Button>(() => ({
-		id: 'cancel',
-		label: 'Cancelar',
-		icon: 'cancel',
-		buttonType: 'stroked',
-		htmlType: 'button',
-		disabled: false,
-		action: () => this.onCancelButtonClicked(),
-	}));
-	protected backButton: Button = {
-		id: 'back',
-		icon: 'arrow_back',
-		buttonType: 'icon',
-		htmlType: 'button',
-		disabled: false,
-		action: () => this.onBackButtonClicked(),
-	};
-	protected ownerButton: Button = {
-		id: 'owner',
-		label: 'Propietario',
-		icon: 'badge',
-		buttonType: 'flat',
-		htmlType: 'button',
-		disabled: false,
-		action: () => {},
-	};
-
-	constructor() {
-		effect(() => {
-			this.loadingService.show(this.loadingRef);
-			const user = this.authStore.user$();
-			if (user) {
-				this.user = user;
-				this.nombreControl.setValue(user.nombre);
-				this.apellidoControl.setValue(user.apellido);
-				this.emailControl.setValue(user.email);
-				this.loadingService.hide();
-			} else {
-				this.snackbarService.open({
-					message: 'Error al cargar los datos del usuario',
-					type: 'danger',
-					duration: 5000,
-				});
-			}
-		});
-		effect(() => {
-			this.username = this.authStore.username$();
-		});
-
-		this.nombreControl.valueChanges
-			.pipe(takeUntil(this.destroy$))
-			.subscribe(() => this.updateFormValidity());
-		this.apellidoControl.valueChanges
-			.pipe(takeUntil(this.destroy$))
-			.subscribe(() => this.updateFormValidity());
-		this.emailControl.valueChanges
-			.pipe(takeUntil(this.destroy$))
-			.subscribe(() => this.updateFormValidity());
-	}
-
-	private updateFormValidity() {
-		this.isFormValid.set(
-			this.nombreControl.valid &&
-				this.apellidoControl.valid &&
-				this.emailControl.valid
-		);
-	}
-
-	onBackButtonClicked() {
-		this.navigationService.goBack();
-	}
-
-	onSaveButtonClicked() {
-		if (this.isFormValid()) {
-			const nombre = this.nombreControl.value!;
-			const apellido = this.apellidoControl.value!;
-			const email = this.emailControl.value!;
-			const request: UpdateUserRequest = {
-				id: this.user!.id,
-				nombre,
-				apellido,
-				email,
-			};
-			this.loadingService.show(this.loadingRef);
-			this.authService
-				.updateProfile(request)
-				.pipe(takeUntil(this.destroy$))
-				.subscribe({
-					next: (user) => {
-						this.authStore.setState({ user });
-						this.loadingService.hide();
-						this.snackbarService.open({
-							message: 'Perfil actualizado correctamente',
-							type: 'success',
-						});
-					},
-					error: (error) => {
-						this.loadingService.hide();
-						this.snackbarService.open({
-							message: error.message
-								? error.message
-								: 'Error al actualizar el perfil',
-							type: 'danger',
-							duration: 5000,
-						});
-					},
-				});
-		}
-	}
-
-	onCancelButtonClicked() {
-		if (this.user) {
-			this.nombreControl.setValue(this.user.nombre);
-			this.apellidoControl.setValue(this.user.apellido);
-			this.emailControl.setValue(this.user.email);
-		}
-	}
-
-	isOwner(): boolean {
-		return this.user?.roles.includes(UserRoles.Owner) || false;
-	}
+	protected isOwner = true;
 }
